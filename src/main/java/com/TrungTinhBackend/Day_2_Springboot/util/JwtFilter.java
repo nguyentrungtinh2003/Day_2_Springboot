94231477e0652a89c671db0b3469cf1798126ad4
nguyentrungtinh <trungtinhn300@gmail.com>
2025-02-05 09:40:21 +0700

    Update func doFilter jwt

--- src/main/java/com/TrungTinhBackend/Day_2_Springboot/util/JwtFilter.java
+++ src/main/java/com/TrungTinhBackend/Day_2_Springboot/util/JwtFilter.java
@@ -5,10 +5,12 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class JwtFilter extends OncePerRequestFilter {
 
@@ -17,9 +19,19 @@ public class JwtFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String token = request.getHeader("authorization");
-        if(token != null && jwtUtil.validateToken(token.replace("Bearer ", ""))) {
-            SecurityContextHolder.getContext().setAuthentication(null);
+        String token = request.getHeader("Authorization");
+
+        if(token != null && token.startsWith("Bearer ")) {
+            token = token.substring(7);
+            if(jwtUtil.validateToken(token)) {
+                String username = jwtUtil.extractUsername(token);
+
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                        username,null,new ArrayList<>()
+                );
+
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
         }
         filterChain.doFilter(request,response);
     }
